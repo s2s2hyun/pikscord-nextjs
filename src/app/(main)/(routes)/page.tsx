3f74c7e80f0a7b957dd8300d9a3ba8b7abfa9020
s2s2hyun <s2s2hyun0601@gmail.com>
2024-03-09 16:12:35 +0900

    next-theme , Clerk auth , nextjs-옵셔널 캐치 - 올라우트 페이지 분별 추가완료

--- src/app/(main)/(routes)/page.tsx
+++ src/app/(main)/(routes)/page.tsx
@@ -1,12 +1,16 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import { ModeToggle } from "@/components/toggle/mode-toggle";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="h-full flex  flex-col items-center justify-center">
       <p className="text-3xl font-bold text-indigo-500">Hello Pikscord </p>
-      <Button variant="destructive">Click me</Button>
+      <Button variant="test">Click me</Button>
+      <ModeToggle />
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
